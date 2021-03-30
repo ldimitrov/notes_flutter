9e7dc2c42fb75a4b4c74036a47bad1c40befc93b
ldimitrov <ldimitrov070@gmail.com>
2021-03-30 23:24:08 +0200

    model sign-in form events and state with bloc

---
+++ lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_flutter/domain/auth/auth_failure.dart';
+import 'package:notes_flutter/domain/auth/authentication.dart';
+import 'package:notes_flutter/domain/auth/i_auth_facade.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: Implement
+  }
+}
--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -3,11 +3,15 @@ import 'package:notes_flutter/domain/auth/auth_failure.dart';
 import 'package:notes_flutter/domain/auth/authentication.dart';
 
 abstract class IAuthFacade {
-  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
-      {required EmailAddress emailAddress, required Password password});
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
 
-  Future<Either<AuthFailure, Unit>> signIn(
-      {required EmailAddress emailAddress, required Password password});
+  Future<Either<AuthFailure, Unit>> signIn({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
 
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
