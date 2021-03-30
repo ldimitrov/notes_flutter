87fcddd41e923feef0852d23ad3b833abedb70b3
ldimitrov <ldimitrov070@gmail.com>
2021-03-30 22:33:54 +0200

    added interface for authentication functionality

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:notes_flutter/domain/auth/auth_failure.dart';
+import 'package:notes_flutter/domain/auth/authentication.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signIn(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
